5e8155d63fffbd4b565c096992e3e6f72821c3a3
thomkeh <thomas5max@gmail.com>
2020-06-24 15:29:24 +0100

    Add probability and percentage and documentation (#3)
    * Add probability and percentage and documentation
    
    * Format with black

---
+++ docsource/tee.constraints.rst
@@ -0,0 +1,6 @@
+tee.constraints
+====================
+
+.. automodule:: tee.constraints
+   :members:
+   :imported-members:
--- tests/constraints_test.py
+++ tests/constraints_test.py
@@ -1,7 +1,7 @@
 """Tests for constraints."""
 from pytest import raises
 
-from tee import Fraction, NaturalNum, PositiveInt
+from tee import Fraction, NaturalNum, Percentage, PositiveInt, Probability
 
 
 def test_natural_num() -> None:
@@ -26,3 +26,19 @@ def test_fraction() -> None:
 
     with raises(AssertionError):
         Fraction(1.2)
+
+
+def test_percentage() -> None:
+    x: Percentage = Percentage(1.0)
+    assert isinstance(x, float)
+
+    with raises(AssertionError):
+        Percentage(1.2)
+
+
+def test_probability() -> None:
+    x: Probability = Probability(0.0)
+    assert isinstance(x, float)
+
+    with raises(AssertionError):
+        Probability(10)
