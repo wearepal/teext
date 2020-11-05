099cf908cffffce6f3b3bc1d6506ae4ed850059e
Thomas M <thomas5max@gmail.com>
2020-11-05 17:52:53 +0000

    Add result type

---
+++ teext/result.py
@@ -0,0 +1,26 @@
+from typing import Generic, TypeVar, Union
+
+__all__ = ["Err", "Ok", "Result"]
+
+
+T = TypeVar("T")
+E = TypeVar("E", bound=Exception)
+
+
+class Ok(Generic[T]):
+    def __init__(self, value: T) -> None:
+        self._value = value
+
+    def ok(self) -> T:
+        return self._value
+
+
+class Err(Generic[E]):
+    def __init__(self, e: E) -> None:
+        self._e = e
+
+    def err(self) -> E:
+        return self._e
+
+
+Result = Union[Ok[T], Err[E]]
