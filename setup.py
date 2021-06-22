363e9b1d5b05d4e48465bd4981e60ba2196be4a9
Thomas M <thomas5max@gmail.com>
2021-06-22 11:43:28 +0100

    Bump version to 0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="teext",
-    version="0.1.2",
+    version="0.1.3",
     author="PAL",
     author_email="info@predictive-analytics-lab.com",
     description="Typing extensions extensions",
